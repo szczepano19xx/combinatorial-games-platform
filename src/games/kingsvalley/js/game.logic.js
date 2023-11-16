ebc7172c5088a888d93d1610c6ea83bd3689fcbb
f1shu <szulzyckifilip@gmail.com>
2023-11-16 10:18:49 +0100

    Create game folder and add basic files

---
+++ src/games/kingsvalley/js/game.logic.js
@@ -0,0 +1,31 @@
+const boardWidth = 5;
+const boardHeight = 5;
+
+const logicOfGame = {
+    /**
+     * Generuje stan początkowy gry.
+     */
+    generateInitialState() {},
+    /**
+     * Funkcja oceny, która ocenia z punktu widzenia wskazanego gracza.
+     */
+    evaluateState(state, player) {},
+    /**
+     * Funkcja generująca możliwe ruchy z wskazanego stanu dla gracza.
+     */
+    generateMoves(state, player) {},
+    /**
+     * Funkcja generuje stan po wykonaniu wskazanego ruchu.
+     */
+    generateStateAfterMove(previousState, player, move) {},
+    /**
+     * Funkcja sprawdza czy stan jest terminalny, tzn. koniec gry.
+     */
+    isStateTerminal(state, player) {},
+    /**
+     * Funkcja generująca unikalny klucz dla wskazanego stanu.
+     */
+    generateUniqueKey: undefined,
+};
+
+const players = [];
